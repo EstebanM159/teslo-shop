ebf0ff9fa831833d1ffd9b112bcf984d1a0f6ca5
EstebanM159 <estebanm91231@gmail.com>
2025-09-07 02:14:32 -0300

    Cambio de link de api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  baseUrl: 'https://nest-teslo-shop-backend-g5vf.onrender.com/api',
+  baseUrl: 'https://nest-teslo-shop-backend-y8ev.onrender.com/api',
   baseImageUrl:
     'https://lmujuiyquyiicjomqomi.supabase.co/storage/v1/object/public/test/products',
 };
